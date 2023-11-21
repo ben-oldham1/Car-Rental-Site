fa6c992f529510863adc284ef34aa1dd71a496af
Ben <ben.oldham2003@gmail.com>
2023-11-21 22:04:50 +0000

    Add new fields

--- src/app/components/ReservationForm.js
+++ src/app/components/ReservationForm.js
@@ -16,80 +16,124 @@ const ReservationForm = ({ initialValues }) => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-                <label htmlFor="start" className="form-label">Start Location</label>
-                <select
-                    className="form-select"
-                    id="start"
-                    name="start"
-                    value={formData.start}
-                    onChange={handleChange}
-                >
-                    <option value='' disabled={true}>
+        <section className='card'>
+            <div className='card-body'>
 
-                    </option>
-                    {branchData.map((branch) => (
-                        <option key={branch.key} value={branch.name} selected={branch.name == formData.start ? true : false}>
-                            {branch.name}
-                        </option>
-                    ))}
-                </select>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="end" className="form-label">End Location</label>
-                <select
-                    className="form-select"
-                    id="end"
-                    name="end"
-                    value={formData.end}
-                    onChange={handleChange}
-                >
-                    <option value='' disabled={true}>
+                <form onSubmit={handleSubmit}>
+                    <div className='row'>
+                        <div className="col-md-6 mb-3">
+                            <label htmlFor="start" className="form-label">Start Location</label>
+                            <select
+                                className="form-select"
+                                id="start"
+                                name="start"
+                                value={formData.start}
+                                onChange={handleChange}
+                            >
+                                <option value='' disabled={true}>
 
-                    </option>
-                    {branchData.map((branch) => (
-                        <option key={branch.key} value={branch.name} selected={branch.name == formData.end ? true : false}>
-                            {branch.name}
-                        </option>
-                    ))}
-                </select>
+                                </option>
+                                {branchData.map((branch) => (
+                                    <option key={branch.key} value={branch.name} selected={branch.name == formData.start ? true : false}>
+                                        {branch.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="col-md-6 mb-3">
+                            <label htmlFor="end" className="form-label">End Location</label>
+                            <select
+                                className="form-select"
+                                id="end"
+                                name="end"
+                                value={formData.end}
+                                onChange={handleChange}
+                            >
+                                <option value='' disabled={true}>
+
+                                </option>
+                                {branchData.map((branch) => (
+                                    <option key={branch.key} value={branch.name} selected={branch.name == formData.end ? true : false}>
+                                        {branch.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div className="col-md-4 mb-3">
+                            <label htmlFor="pickup" className="form-label">Pickup Date</label>
+                            <input
+                                type="date"
+                                className="form-control"
+                                id="pickup"
+                                name="pickup"
+                                value={formData.pickup}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="col-md-4 mb-3">
+                            <label htmlFor="drop" className="form-label">Drop Date</label>
+                            <input
+                                type="date"
+                                className="form-control"
+                                id="drop"
+                                name="drop"
+                                value={formData.drop}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="col-md-4 mb-3">
+                            <label htmlFor="car" className="form-label">Car ID</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="car"
+                                name="car"
+                                value={formData.car}
+                                onChange={handleChange}
+                            />
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div className='col-12 mb-3'>
+                            <label htmlFor='note'>Booking note</label>
+                            <textarea
+                                className="form-control"
+                                rows="2"
+                                id='note'
+                                name='note'
+                                onChange={handleChange}
+                            ></textarea>
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div className='col-12 mb-3'>
+                            <div className="col-12 mb-3">
+                                <label htmlFor="email" className="form-label">Your email</label>
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    id="email"
+                                    name="email"
+                                    onChange={handleChange}
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div className='col-12 mb-3'>
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                        </div>
+                    </div>
+                </form>
+
             </div>
-            <div className="mb-3">
-                <label htmlFor="pickup" className="form-label">Pickup Date</label>
-                <input
-                    type="date"
-                    className="form-control"
-                    id="pickup"
-                    name="pickup"
-                    value={formData.pickup}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="drop" className="form-label">Drop Date</label>
-                <input
-                    type="date"
-                    className="form-control"
-                    id="drop"
-                    name="drop"
-                    value={formData.drop}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="car" className="form-label">Car ID</label>
-                <input
-                    type="text"
-                    className="form-control"
-                    id="car"
-                    name="car"
-                    value={formData.car}
-                    onChange={handleChange}
-                />
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+        </section>
     );
 };
 
