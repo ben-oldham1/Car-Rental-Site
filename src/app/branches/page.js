e5ed35d5f3dd02278e0897f112c2f53906eff702
Ben <ben.oldham2003@gmail.com>
2023-11-21 13:53:17 +0000

    Added content

--- src/app/branches/page.js
+++ src/app/branches/page.js
@@ -1,5 +1,19 @@
-import Link from 'next/link';
+import branchData from '../data/Branches.json';
+import BranchCard from '../components/BranchCard';
 
 export default function Page() {
-    return <h1>Branches page- Go to <Link href="/home">home</Link></h1>
+    return (
+        <div className='container'>
+            <h1>Our branches</h1>
+
+            <div className='row'>
+                {branchData.map((branch) => (
+                    <div className='col-md-6 mb-3'>
+                        <BranchCard key={branch.key} branch={branch} />
+                    </div>
+                ))}
+            </div>
+        </div>
+
+    )
 }
