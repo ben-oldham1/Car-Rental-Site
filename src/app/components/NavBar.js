67100362856ee519ec5d656601f8df6818f70adb
Ben <ben.oldham2003@gmail.com>
2023-11-21 13:53:58 +0000

    Make active link work

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,9 +1,14 @@
+"use client"
+
 import React from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation'
 
 function Navigation() {
+    const pathname = usePathname()
+
     return (
-        <nav className="navbar navbar-expand-lg bg-white">
+        <nav className="navbar navbar-expand-lg py-0 bg-white">
             <div className="container-fluid">
                 <Link href="/home" className="navbar-brand">
                     Car rent name
@@ -22,23 +27,30 @@ function Navigation() {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <Link href="/home" className="nav-link active">
+                            <Link href="/home" className={`nav-link p-3 ${pathname === '/home' ? 'active' : ''}`}>
                                 Instant quote
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link href="/fleet" className="nav-link">
+                            <Link href="/fleet" className={`nav-link p-3 ${pathname === '/fleet' ? 'active' : ''}`}>
                                 Our fleet
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link href="/branches" className="nav-link">
+                            <Link href="/branches" className={`nav-link p-3 ${pathname === '/branches' ? 'active' : ''}`}>
                                 Branches
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link href="/contact" className="nav-link">
+                            <Link href="/contact" className={`nav-link p-3 ${pathname === '/contact' ? 'active' : ''}`}>
                                 Contact
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href="/reserve">
+                                <button className='btn btn-primary'>
+                                    Reserve
+                                </button>
                             </Link>
                         </li>
                     </ul>
